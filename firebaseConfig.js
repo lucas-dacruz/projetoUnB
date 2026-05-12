087b3c549738e5f6683279cee3542cb3bbf15d3f
lucas-dacruz <lucasarrudadc@gmail.com>
2026-05-12 15:23:27 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.APP_ID,
-  projectId: process.env.AUTH_DOMAIN,
-  storageBucket: process.env.MEASUREMENT_ID,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.PROJECT_ID,
-  measurementId: process.env.STORAGE_BUCKET
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_APP_ID,
+  projectId: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  storageBucket: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  measurementId: process.env.EXPO_PUBLIC_STORAGE_BUCKET
 };
 
 const app = initializeApp(firebaseConfig);
