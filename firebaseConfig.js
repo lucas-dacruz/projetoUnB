45a0b87d8c8fc00ede5fe4c8c9ceea38553ccaee
lucas-dacruz <lucasarrudadc@gmail.com>
2026-05-12 16:38:25 -0300

    Reordanate envs

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
+
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_APP_ID,
-  projectId: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
-  storageBucket: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  measurementId: process.env.EXPO_PUBLIC_STORAGE_BUCKET
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
